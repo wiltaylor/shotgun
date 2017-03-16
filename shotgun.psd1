f0b5c1ffa733e3073afe37bca8b5d3832cc8c04a
Wil Taylor <wilfridtaylor@gmail.com>
2017-03-17 00:54:57 +1100

    Inital Version

---
+++ shotgun.psd1
@@ -0,0 +1,56 @@
+@{
+
+# Script module or binary module file associated with this manifest.
+ModuleToProcess = 'shotgun.psm1'
+
+# Version number of this module.
+ModuleVersion = '1.0.0'
+
+# ID used to uniquely identify this module
+GUID = '5505a4d4-0a44-42b5-a724-b5245cdddebd'
+
+# Author of this module
+Author = 'Wil Taylor'
+
+# Copyright statement for this module
+Copyright = 'Copyright (c) 2017 by Wil Taylor.'
+
+# Description of the functionality provided by this module
+Description = 'Simple module which allows easy re-run the same commands. Mostly useful for development cycles.'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '2.0'
+
+# Functions to export from this module
+FunctionsToExport = @( 
+    'Start-Shotgun'
+)
+
+# # Aliases to export from this module
+AliasesToExport = 'shotgun'
+
+PrivateData = @{
+    # PSData is module packaging and gallery metadata embedded in PrivateData
+    # It's for rebuilding PowerShellGet (and PoshCode) NuGet-style packages
+    # We had to do this because it's the only place we're allowed to extend the manifest
+    # https://connect.microsoft.com/PowerShell/feedback/details/421837
+    PSData = @{
+        # The primary categorization of this module (from the TechNet Gallery tech tree).
+        Category = "Tool"
+
+        # Keyword tags to help users find this module via navigations and search.
+        Tags = @('shotgun')
+
+
+        # Indicates this is a pre-release/testing version of the module.
+        IsPrerelease = 'False'
+    }
+}
+
+# HelpInfo URI of this module
+# HelpInfoURI = ''
+
+# Default prefix for commands exported from this module. Override the default prefix using Import-Module -Prefix.
+# DefaultCommandPrefix = ''
+
+}
---
+++ shotgun.psm1
@@ -0,0 +1,110 @@
+<#
+	.SYNOPSIS
+	Allows the same block of script to be run multiple times.
+	.DESCRIPTION
+    This cmdlet lets the user trigger the a script block over and over again by simply pressing enter.
+
+    This is really useful for development work flows where you need to run the same commands again and again.
+
+    The commandlet can run in 2 modes. First mode is where you pass a script block via the Code parameter. This
+    will simply just let the user press enter to rerun the script block. The second mode will read a shotgun.psd1 file
+    from the current directory and present the user with options specified in that file.
+
+    An example file layout is as follows:
+
+    @{
+        Action1 = {
+            Write-Host "Action 1 ran"
+        }
+
+        Action2 = {
+            Write-Host "Action 2 ran"
+        }
+
+        Action3 = {
+            Write-Host "Action 3 ran"
+        }
+    }
+
+
+
+	.PARAMETER Code
+	Code block to run.
+#>
+function Start-Shotgun 
+{
+    [CmdletBinding()]
+    param([Parameter(Mandatory = $false, ValueFromPipeline = $true)][ScriptBlock]$Code)
+
+    $defaultOption = -1
+
+    while($true) 
+    {
+        if($code -ne $null) 
+        {
+            &$Code
+
+            $result = Read-Host "Press enter to rerun commands. Q or Ctrl + C to quit"
+
+            if($result.ToLower() -eq "q") 
+            {
+                return
+            }
+        }
+        else
+        {
+            if(-not(Test-Path ".\shotgun.psd1")) 
+            {
+                Write-Host "Please either use -Code switch or a shotgun.psd1 file"
+                return
+            }
+
+            $options = Get-Content .\shotgun.psd1 -raw | Invoke-Expression
+            $index = 0
+            $scripts = @()
+            foreach($i in ($options.Keys | Sort-Object -Descending)) 
+            {
+                Write-Host "[$index] - $i"
+                $scripts += $options[$i]
+                $index++
+            }
+
+            Write-Host "[q] - Quit shotgun"
+
+            $selectedoption = -1
+
+            while($selectedoption -eq -1 -or $selectedoption -ge $index) 
+            {
+                $inputtext = ""
+
+                if($defaultOption -eq -1) 
+                {
+                    $inputtext = Read-Host ">"
+                } 
+                else
+                {
+                    $inputtext = Read-Host "[$defaultOption]>"
+                }
+                
+                if($inputtext.ToLower() -eq "q") 
+                {
+                    return
+                }                
+
+                if($inputtext -eq "")
+                {
+                    $selectedoption = [int]::Parse($defaultOption)
+                }
+                else
+                {
+                    $selectedoption = [int]::Parse($inputtext)
+                }
+            }
+
+            $defaultOption = $selectedoption
+            &$scripts[$selectedoption]
+        }
+    }
+}
+
+New-Alias -Name shotgun -Value Start-Shotgun
